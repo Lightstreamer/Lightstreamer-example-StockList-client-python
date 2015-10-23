d5dc47e6ebe983571ae5b988d14abb3adc10f8ca
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2015-10-23 13:14:25 +0200

    Update copyright information.

--- src/stock_list_demo.py
+++ src/stock_list_demo.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python
 
-#  Copyright 2014 Weswit s.r.l.
+# Copyright (c) Lightstreamer Srl. All rights reserved.
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+# Lightstreamer Srl has intellectual property rights relating to
+# technology embodied in this product.
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# This product is distributed under licenses restricting its use, 
+# copying distribution, and decompilation. No part of this product may 
+# be reproduced in any form by any means without prior written 
+# authorization of Lightstreamer Srl and its licensors, if any.
 
 import sys
 import logging
